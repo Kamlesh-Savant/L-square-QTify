1a81d0ddf2b9c1c43bc25a41a33f645439498cd0
Kamlesh Savant <sawantk45@gmail.com>
2024-06-14 12:52:33 +0530

    updated card section for adding new album section below top album section

--- qtify/src/component/Section/Section.jsx
+++ qtify/src/component/Section/Section.jsx
@@ -4,33 +4,58 @@ import styles from './Section.module.css';
 import axios from 'axios';
 
 function Section() {
-  const [data, setData] = useState([]);
+  const [dataTop, setDataTop] = useState([]);
+  const [dataNew, setDataNew] = useState([]);
 
-  const getAlbums = async () => {
+
+  const getTopAlbums = async () => {
+    try {
+        const res = await axios.get('https://qtify-backend-labs.crio.do/albums/top');
+        setDataTop(res.data);
+    } catch (err) {
+        console.log("Error fetching albums:", err);
+    }
+};
+
+  const getNewAlbums = async () => {
       try {
-          const res = await axios.get('https://qtify-backend-labs.crio.do/albums/top');
-          setData(res.data);
+          const res = await axios.get('https://qtify-backend-labs.crio.do/albums/new');
+          setDataNew(res.data);
       } catch (err) {
           console.log("Error fetching albums:", err);
       }
   };
 
   useEffect(() => {
-      getAlbums();
+      getTopAlbums();
+      getNewAlbums();
   }, []);
 
   return (
+    <>
     <div className={styles.wrapper}>
         <div className={styles.topSection}>
             <h3>Top Albums</h3>
             <button className={styles.btnShowAll}>Show all</button>
         </div>
         <div className={styles.contentSection}>
-            {data.map((album) => (
+            {dataTop.map((album) => (
                 <Card key={album.id} data={album} type='album' />
             ))}
         </div>
     </div>
+    <div className={styles.wrapper}>
+        <div className={styles.topSection}>
+            <h3>New Albums</h3>
+            <button className={styles.btnShowAll}>Show all</button>
+        </div>
+        <div className={styles.contentSection}>
+            {dataNew.map((album) => (
+                <Card key={album.id} data={album} type='album' />
+            ))}
+        </div>
+    </div>
+    </>
 );
   
 }
