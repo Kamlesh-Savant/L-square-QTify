c04b5c9e38030a3914f2d71dc651b5c449891fac
Kamlesh Savant <sawantk45@gmail.com>
2024-06-17 11:46:12 +0530

    Change in space between of carousel

--- qtify/src/component/Carousel/Carousel.jsx
+++ qtify/src/component/Carousel/Carousel.jsx
@@ -25,8 +25,8 @@ function Carousel({data, renderComponent}) {
         style={{padding:"0px 20px"}}
         initialSlide={0}
         modules={[Navigation]}
-        slidesPerView={'auto'}
-        spaceBetween={30}
+        slidesPerView={"auto"}
+        spaceBetween={40}
         allowTouchMove >
           <Controls data={data} />
           <CrouselLeftNavigation />
